1367a718b3c02862598258df5cd914f648c892ad
richard jarram <rjarram@me.com>
2020-09-23 10:39:07 -0400

    built initial contract deploy test script with ganache-cli

---
+++ README.md
@@ -0,0 +1,3 @@
+### Work in Progress
+#### Important Points
+* ganache-cli is only compatible with Node version 12 or less.
--- test/inbox_test.js
+++ test/inbox_test.js
@@ -1,5 +1,25 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  // get eth accounts from web3 lib
+  accounts = await web3.eth.getAccounts();
+
+  // use an account to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: bytecode, arguments: ['Initial messgae passed to inbox smart contract... Hi!']})
+    .send({ from: accounts[0], gas: '1000000' })
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    console.log('account: ', accounts);
+    console.log('inbox: ', inbox);
+  });
+});

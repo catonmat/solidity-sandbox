3fb0d9989739fd275c712c03ac1279fee72e3201
richard jarram <rjarram@me.com>
2020-10-05 13:18:48 -0400

    pre web3 update

--- deploy.js
+++ deploy.js
@@ -1,26 +1,24 @@
 const dotenv = require("dotenv").config();
-
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
 const provider = new HDWalletProvider(
   process.env.WALLET_SEED,
-  process.env.INUFRA_ENDPOINT
+  process.env.INFURA_ENDPOINT
 );
-
-const web3 = new Web3(provider);
+const web3 = new Web3(provider, 42);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
-  console.log('Attempting to deploy from account', accounts[0]);
+  console.log('Attempting to deploy from account', accounts);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ['Hi there!']})
-    .send({ 'gas': '1000000', from: accounts[0] });
+    .send({ 'gas': '67072', from: accounts[0] });
 
-  console.log('Contract deployed to: ', results.options.address);
+  console.log('Contract deployed to: ', result.options.address);
 }
 
 deploy();

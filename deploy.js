ac58e15f26be2dd0b8ca70d05d99d164dd76b2a6
richard jarram <rjarram@me.com>
2020-10-05 09:07:27 -0400

    edited deploy script

--- deploy.js
+++ deploy.js
@@ -3,5 +3,8 @@ const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
 const provider = new HDWalletProvider(
-
+  'mnenomic',
+  'https://kovan.infura.io/v3/41b8297a96814900b29adb99d458a2f5'
 );
+
+console.log(process.env.CRYPTO_SEED_BRAVE_WALLET)
